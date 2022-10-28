165878beb6b3cc59aa57f5138598277dd9bd590f
Trung Quan <quanb1910130@student.ctu.edu.vn>
2022-10-28 16:04:40 +0700

    Xay dung trang gio hang

--- lib/main.dart
+++ lib/main.dart
@@ -1,31 +1,30 @@
 import 'package:flutter/material.dart';
-
+import 'ui/products/product_detail_screen.dart';
+import 'ui/products/Products_Manager.dart';
+import 'ui/products/product_overview_screen.dart';
 import 'ui/products/user_products_screen.dart';
+import 'ui/cart/cart_screen.dart';
 
 void main() {
   runApp(const MyApp());
 }
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'Myshop',
-        debugShowCheckedModeBanner: false,
-        theme: ThemeData(
-          fontFamily: 'Lato',
-          colorScheme: ColorScheme.fromSwatch(
-            primarySwatch: Colors.blue,
-          ).copyWith(
-            secondary: Colors.deepOrange,
-          ),
+      title: 'My Shop',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        fontFamily: 'Lato',
+        colorScheme: ColorScheme.fromSwatch(
+          primarySwatch: Colors.purple,
+        ).copyWith(
+          secondary: Colors.deepOrange,
         ),
-        // home: Container(
-        //   color: Colors.green,
-        // ));
-        home: const SafeArea(
-          child: UserProductsScreen(),
-        ));
-  }
-}
+      ),
+      home: SafeArea(
+        child: CartScreen(),
+      ),
+    );
+  }}
