bcbbbf2a227f1ecb6b016ca38821156ed2c1edf6
Trung Quan <quanb1910130@student.ctu.edu.vn>
2022-10-29 09:25:58 +0700

    hoan thanh lab 2

--- lib/models/order_item.dart
+++ lib/models/order_item.dart
@@ -1,24 +1,23 @@
+import 'package:myshop/models/product.dart';
 import 'cart_item.dart';
-
 class OrderItem {
   final String? id;
   final double amount;
   final List<CartItem> products;
   final DateTime dateTime;
-
-  int get productCount {
-    return products.length;
-  }
-
   OrderItem({
     this.id,
     required this.amount,
     required this.products,
     DateTime? dateTime,
   }) : dateTime = dateTime ?? DateTime.now();
+  int get productCount {
+    return products.length;
+  }
 
   OrderItem copyWith({
     String? id,
+    double? amout,
     double? amount,
     List<CartItem>? products,
     DateTime? dateTime,
